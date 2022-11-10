f061a3f47e781ea47cab6e6b3e40b1ed6248fce7
numangit <abdulnumanjob@gmail.com>
2022-11-10 17:51:47 +0600

    services api is created

--- index.js
+++ index.js
@@ -15,7 +15,12 @@ const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology:
 async function run() {
     try {
         const serviceCollection = client.db('snap').collection('services');
-
+        app.get('/services', async (req, res) => {
+            const query = {}
+            const cursor = serviceCollection.find(query);
+            const services = await cursor.toArray();
+            res.send(services);
+        });
     }
     finally {
     }
