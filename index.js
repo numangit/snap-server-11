9619c4a1739680bcdf2ce9d59ac33d38e7307b47
numangit <abdulnumanjob@gmail.com>
2022-11-10 20:56:03 +0600

    my reviews api crated using query param

--- index.js
+++ index.js
@@ -39,6 +39,18 @@ async function run() {
         });
 
         //reviews api
+        app.get('/myReviews', async (req, res) => {
+            let query = {};
+            if (req.query.email) {
+                query = {
+                    email: req.query.email
+                }
+            }
+            const cursor = reviewCollection.find(query);
+            const orders = await cursor.toArray();
+            res.send(orders);
+        });
+
         app.post('/reviews', async (req, res) => {
             const review = req.body;
             const result = await reviewCollection.insertOne(review);
