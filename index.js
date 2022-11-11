0bf08396c58392ee1401454a5bee3e04873d78c9
numangit <abdulnumanjob@gmail.com>
2022-11-11 08:18:22 +0600

    add service api added

--- index.js
+++ index.js
@@ -38,6 +38,12 @@ async function run() {
             res.send(service);
         });
 
+        app.post('/services', async (req, res) => {
+            const service = req.body;
+            const result = await serviceCollection.insertOne(service);
+            res.send(result);
+        });
+
         //reviews api
         app.get('/myReviews', async (req, res) => {
             let query = {};
